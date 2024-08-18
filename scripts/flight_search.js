ea6a0d74cb26079b262bd34029e35bdce8eb2d5d
lexuanbinh <lb@silverliningresearch.com>
2024-08-18 22:45:11 +0700

    update new files

--- scripts/flight_search.js
+++ scripts/flight_search.js
@@ -64,7 +64,7 @@ function notDeparted_flight_search(flight_time) {
 }
 
 function load_flight_list() {
-  flightRawList = JSON.parse(MUC_Departures_Flight_List_Raw);
+  flightRawList = JSON.parse(JED_Departures_Flight_List_Raw);
   flightList = [];
   flightList.length = 0;
   flightShortList = [];
